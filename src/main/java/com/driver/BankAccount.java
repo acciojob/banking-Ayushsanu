ec99a96abc52f73a5885f247e80e4eff99d244cd
Ayushsanu <107394283+Ayushsanu@users.noreply.github.com>
2023-06-17 17:42:47 +0530

    solution added

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,57 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance  = balance;
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
+        int rem = sum;
+        String accountNo = "";
+        if(digits*9 < sum){
+            throw new Exception("Account Number can not be generated");
+        }
+        else {
+            while (digits > 0 && rem > 0) {
+                if (rem >= 9) {
+                    rem = rem - 9;
+                    accountNo = accountNo + "9";
+                } else {
+                    accountNo = accountNo + Integer.toString(rem);
+                    rem = 0;
+                }
+                digits--;
+            }
+            while (digits > 0) {
+                accountNo = accountNo + "0";
+                digits--;
+            }
+            return accountNo;
+        }
         return null;
+
+
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        balance += amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        if(this.balance-amount >= getMinBalance()){
+            balance= balance - amount;
 
+        }
+        else{
+            throw  new Exception("Insufficient Balance");
+        }
     }
 
 }
